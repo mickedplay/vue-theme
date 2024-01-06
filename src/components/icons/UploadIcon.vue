397a7451bc0cfa7ce15fda340eae112e5690403b
mickedplay <mickedplay@gmail.com>
2024-01-07 00:07:32 +0100

    Updated

---
+++ src/components/icons/UploadIcon.vue
@@ -0,0 +1,13 @@
+<template>
+    <Icon :icon="['fas', 'cloud-arrow-up']" v-bind="$attrs"/>
+</template>
+
+<script>
+import Icon from './Icon.vue';
+
+export default {
+    components: {
+        Icon
+    }
+};
+</script>
--- src/components/scaffolds/Backend.vue
+++ src/components/scaffolds/Backend.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="app-container w-full h-screen grid overflow-hidden default-transition
-                grid-cols-[4rem,1fr] lg:grid-cols-[20rem,1fr] grid-rows-[4rem,1fr,5rem] lg:grid-rows-[4rem,1fr,3rem]"
+                grid-cols-[4rem,1fr] lg:grid-cols-[16rem,1fr] grid-rows-[4rem,1fr,5rem] lg:grid-rows-[4rem,1fr,3rem]"
     >
         <slot name="logo"/>
         <slot name="topbar"/>
